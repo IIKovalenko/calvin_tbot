4064a35e8f686e62b908fbbe2755c4f06d8912ec
Muslim Abdulmazhidov <htrx101@gmail.com>
2019-02-26 17:14:53 +0300

    add planet_info
    test func planet_info

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import ephem
 
 import settings
 
@@ -14,9 +15,24 @@ logging.basicConfig(
 
 
 def start_msg(bot, update):
-    text = 'Привет мой друг, я Кельвин, виртуальный образец инопланетного существа из фильма "Живое".'
+    text = 'Привет, мой друг, я - Кельвин, виртуальный образец инопланетного существа из фильма "Живое".'
     logging.info(text)
     update.message.reply_text(text)
+
+
+def help_msg(bot, update):
+    help_text = """
+    Доспуные команды:
+    
+    /help - вызывает это сообщение
+    /planet <имя планеты> - получите информацию по планете (доступна только Солнечная ситема)
+    /start - показывает приветсвтие
+    
+    
+    PS.: По мере добавления команд help будет пополняться.
+     """
+    logging.info('Вызвана команда /help')
+    update.message.reply_text(help_text)
 
 
 def calvin_talk(bot, update):
@@ -27,12 +43,31 @@ def calvin_talk(bot, update):
         'full_name': update.message.chat.first_name + ' ' + update.message.chat.last_name
     }
     user_text = update.message.text
-    print('{} - {} пишет: {}'.format(update.message.date, t_user['full_name'], update.message.text))
     logging.info(
         "User: %s(%s), Chat ID: %s, Message: %s",
         t_user['username'], t_user['full_name'], update.message.chat.id, update.message.text
     )
+    # для теста (на проде этот принт уйдёт)
+    print('{} - {} пишет: {}'.format(update.message.date, t_user['full_name'], update.message.text))
     update.message.reply_text('Вы написали: {}'.format(user_text))
+
+
+def planet_info(bot, update):
+    """Получение информации о планете с помощью модуля ephem"""
+    logging.info('Вызвана команда /planet')
+    astro_obj = update.message.text.split()
+    p_name = str(astro_obj[1])
+    astro_obj = getattr(ephem, astro_obj[1])
+    planet = astro_obj()
+    planet.compute()
+    zodiac = ephem.constellation(planet)
+    bot.send_message(
+        chat_id=update.message.chat.id,
+        text='Вы выбрали планету {}. Она находится в созвездии {}'.format(
+            planet.name,
+            zodiac[1]
+        )
+    )
 
 
 def bot_worker():
@@ -41,6 +76,8 @@ def bot_worker():
 
     dp = calvin_bot.dispatcher
     dp.add_handler(CommandHandler('start', start_msg))
+    dp.add_handler(CommandHandler('help', help_msg))
+    dp.add_handler(CommandHandler('planet', planet_info))
     dp.add_handler(MessageHandler(Filters.text, calvin_talk))
 
     calvin_bot.start_polling()
